b302324c7303757c774e16374b295af894d3e2ad
MD. AL-AMIN <amin57.cse@gmail.com>
2026-07-08 22:53:59 +0600

    fix: lazy stripe init for vercel serverless

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,4 +1,15 @@
 import Stripe from "stripe";
 import config from "../config";
 
-export const stripe = new Stripe(config.stripe_secret_key)
+const getStripeInstance = (): Stripe => {
+    if (!config.stripe_secret_key) {
+        throw new Error("Stripe secret key is not configured in environment variables");
+    }
+    return new Stripe(config.stripe_secret_key);
+};
+
+export const stripe = new Proxy({} as Stripe, {
+    get(_, prop) {
+        return getStripeInstance()[prop as keyof Stripe];
+    }
+});
